8aac037621b8acad862dd7ef2cddc270aba438ea
behzad <beh.ehap@gmail.com>
2025-07-10 22:07:09 +0330

    feat: enhance health check handler and add JSON utility functions

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -10,6 +10,8 @@ import (
 	"github.com/Behehap/Alberta/internal/store"
 )
 
+const version = "0.0.1"
+
 func main() {
 	cfg := config{
 		addr: env.GetString("ADDR", ":8080"),
@@ -19,6 +21,7 @@ func main() {
 			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
 			maxIdleTime:  env.GetString("DB_MAX_IDLE_CONNS", "15m"),
 		},
+		env: env.GetString("ENV", "development"),
 	}
 
 	db, err := db.New(
