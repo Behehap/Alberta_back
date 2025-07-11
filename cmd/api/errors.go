77dc5e3f1a84103a16ac48d930baebdcb326e32b
behzad <beh.ehap@gmail.com>
2025-07-11 15:32:56 +0330

    refactor: replace direct error responses with application-specific error handling methods

---
+++ cmd/api/errors.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func (app *application) internalServerError(w http.ResponseWriter, r *http.Request, err error) {
+	log.Printf("internak server error: %s path: error:", r.Method, r.URL.Path, err)
+	writeJSONError(w, http.StatusInternalServerError, "the server encountered a problem")
+}
+
+func (app *application) badRequestResponse(w http.ResponseWriter, r *http.Request, err error) {
+	log.Printf("bad request error: %s path: error:", r.Method, r.URL.Path, err)
+	writeJSONError(w, http.StatusInternalServerError, err.Error())
+}
+
+func (app *application) notFoundResponse(w http.ResponseWriter, r *http.Request, err error) {
+	log.Printf("bad request error: %s path: error:", r.Method, r.URL.Path, err)
+	writeJSONError(w, http.StatusNotFound, "not found")
+}
